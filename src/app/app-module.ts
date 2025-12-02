17157a639d18ba7b01b611d9346c2722b1fab5ea
demodogo <macaigm@gmail.com>
2025-12-01 23:29:53 -0300

    init project scaff

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,18 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { AuthModule } from './features/auth/auth-module';
+import { Dashboard } from './features/dashboard/dashboard';
 
 @NgModule({
-  declarations: [
-    App
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners()
-  ],
-  bootstrap: [App]
+  declarations: [App, Dashboard],
+  imports: [BrowserModule, AppRoutingModule, AuthModule],
+  providers: [provideBrowserGlobalErrorListeners()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Login } from '@/app/features/auth/login/login';
+import { Dashboard } from '@/app/features/dashboard/dashboard';
+import { authGuard } from '@/app/core/guards/auth-guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard],
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
